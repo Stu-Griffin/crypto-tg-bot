71cde949931e84f6fded9676c77dd5563e92eea1
Stu-Griffin <maxprogrammer39@gmail.com>
2022-07-07 10:12:56 +0300

    canceled saving data all time

--- src/startBot.js
+++ src/startBot.js
@@ -9,16 +9,16 @@ function startBot(bot) {
 			const binance = await binanceApi();
 			const advCash = await advCashApi("USDT_TRC20", "RUR", "SELL", 1.00);
 			if(binance && advCash) {
-				console.log('got data')
+				console.log('got data');
 				const result = advCash - binance;
 				const request = await axios.get(`${process.env.API_URL}/api/bot-users`);
-				const date = Date.now();
-				saveData({
-					binance:binance,
-					advcash:advCash,
-					result:result
-				});
 				if(result >= 0.15) {
+					saveData({
+						binance:binance,
+						advcash:advCash,
+						result:result
+					});
+					const date = Date.now();
 					request.data.map(el => {
 						const string = `Разница(AdvCash > Binance): ${result} \nЦена на AdvCahs: ${advCash} \nЦена на Binance: ${binance} \nВремя: ${date} \n\n`;
 						bot.telegram.sendMessage(el.chatId, string);
